14a9003ef0675d2b083ff7e74bb973bfd273d737
holly bishop <hollybish10@hotmail.co.uk>
2025-05-28 14:19:40 +0100

    MY Exercise - Building Game Attributes
    Add Platforms, MetaScore, Genres, and Published to the GameDetailsPage.
    
    Added Publisher interface

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,13 +1,17 @@
+import { Genre } from './Genre';
 import { Platform } from './Platform';
+import { Publisher } from './Publishers';
 
 export interface Game {
   id: number;
   slug: string;
   name: string;
   description_raw: string;
+  genres: Genre[];
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
   rating: number;
   rating_top: number;
+  publishers: Publisher[];
 }
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Spinner } from '@chakra-ui/react';
+import { Box, Heading, SimpleGrid, Spinner, Text } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import ExpandableText from '../components/ExpandableText';
 import useGame from '../hooks/useGame';
@@ -15,6 +15,48 @@ const GameDetailPage = () => {
     <Box padding={5}>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <SimpleGrid columns={{ sm: 2, md: 2, lg: 2, xl: 2 }} pt={10} spacing={20}>
+        <Box>
+          <Heading size="md" pb={3} color="gray.500">
+            Platforms
+          </Heading>
+          {game.parent_platforms.map((platform) => (
+            <Text key={platform.platform.id} pb={1}>
+              {platform.platform.name}
+            </Text>
+          ))}
+        </Box>
+        <Box>
+          <Heading size="md" pb={3} color="gray.500">
+            Metascore
+          </Heading>
+          <Box
+            backgroundColor={'green.100'}
+            color="green"
+            borderRadius={10}
+            maxWidth={35}
+            textAlign="center"
+            fontWeight="bold">
+            {game.metacritic}
+          </Box>
+        </Box>
+        <Box>
+          <Heading size="md" pb={3} color="gray.500">
+            Genre
+          </Heading>
+          {game.genres.map((genre) => (
+            <Text key={genre.id}>{genre.name}</Text>
+          ))}
+        </Box>
+        <Box>
+          <Heading size="md" pb={3} color="gray.500">
+            Publishers
+          </Heading>
+          {game.publishers.map((publisher) => (
+            <Text key={publisher.id}>{publisher.name}</Text>
+          ))}
+        </Box>
+      </SimpleGrid>
     </Box>
   );
 };
