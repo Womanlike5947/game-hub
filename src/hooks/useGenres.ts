13cd21a25f5decddbfd2c9913881e8e8a90906b8
holly bishop <hollybish10@hotmail.co.uk>
2025-05-28 11:36:17 +0100

    Refactoring Entities
    Refactoring Entities (Interfaces) and moving them into a Entities folder for generic use

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,13 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
-import genres from '../data/genres';
-import APIClient from '../services/api-client';
 import ms from 'ms';
-
-export interface Genre {
-  id: number;
-  name: string;
-  image_background: string;
-}
+import genres from '../data/genres';
+import { Genre } from '../entities/Genre';
+import APIClient from '../services/api-client';
 
 const apiClient = new APIClient<Genre>('genre');
 
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,13 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
-import platforms from '../data/platforms';
-import APIClient from '../services/api-client';
 import ms from 'ms';
-
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+import platforms from '../data/platforms';
+import { Platform } from '../entities/Platform';
+import APIClient from '../services/api-client';
 
 const apiClient = new APIClient<Platform>('/platforms/lists/parents');
 
