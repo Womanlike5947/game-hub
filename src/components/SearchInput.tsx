1a1d9f496d0028d90324d44cfdc8f0c1e01e1d49
holly bishop <hollybish10@hotmail.co.uk>
2025-05-30 13:44:04 +0100

    22 - Exercise - Fixing the NavBar
    1. Get the NavBar routing to the home page onClick
    
    2. redirect the user to the home pack when submitting searchText in searchInput and then render filtered games.

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,6 +1,7 @@
 import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
 import { useRef } from 'react';
 import { BsSearch } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
 import useGameQueryStore from '../store';
 
 interface Props {
@@ -13,11 +14,14 @@ const SearchInput = () => {
   // A selector is passed to the store to get the function for the setState
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
 
+  const navigate = useNavigate();
+
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
         if (ref.current) setSearchText(ref.current.value);
+        navigate('/');
       }}>
       <InputGroup>
         <InputLeftElement children={<BsSearch />} />
